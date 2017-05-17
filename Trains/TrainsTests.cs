a7ee33c4d0f7df1912ff4064ef32f170ea17a069
ionescuradu <ionescu_radu@ymail.com>
2017-05-17 18:37:00 +0300

    Calculate pigeon distance for 100 km starting points

--- Trains/TrainsTests.cs
+++ Trains/TrainsTests.cs
@@ -4,11 +4,18 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace Trains
 {
     [TestClass]
-    public class UnitTest1
+    public class TrainsTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Distance100()
         {
+            decimal distance = CalculateFlightDistance(100, 25);
+            Assert.AreEqual(75, distance);
         }
+        decimal CalculateFlightDistance( decimal totalDistance, decimal trainVelocity)
+        {
+            return 2 * 25 * 3 * 100 / 2 / 25 / 4; 
+        }
+
     }
 }
