c3fdf67de36494fffcdde391c78bde9afed3883a
ionescuradu <ionescu_radu@ymail.com>
2017-05-17 18:57:38 +0300

    Calculate flight distance for inital 300 km

--- Trains/TrainsTests.cs
+++ Trains/TrainsTests.cs
@@ -12,6 +12,15 @@ namespace Trains
             decimal distance = CalculateFlightDistance(100, 25);
             Assert.AreEqual(75, distance);
         }
+
+        [TestMethod]
+        public void Distance200()
+        {
+            decimal distance = CalculateFlightDistance(300, 25);
+            Assert.AreEqual(225, distance);
+        }
+            
+
         decimal CalculateFlightDistance( decimal totalDistance, decimal trainVelocity)
         {
             decimal flightTime = 3 * totalDistance / trainVelocity / 4 / 2;
