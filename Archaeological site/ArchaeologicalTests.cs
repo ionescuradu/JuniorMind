feeedf4ddbac8b2103ee9783c69ac58b25af314e
ionescuradu <ionescu_radu@ymail.com>
2017-05-18 19:46:02 +0300

    Calculate the area of the building

--- Archaeological site/ArchaeologicalTests.cs
+++ Archaeological site/ArchaeologicalTests.cs
@@ -4,11 +4,19 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace Archaeological_site
 {
     [TestClass]
-    public class UnitTest1
+    public class ArchaeologicalTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void FirstTry()
         {
+            decimal buildingArea = buildingpoints(1.000001m, 2.000002m, 3.000003m, 4.000004m, 6.000006m, 1.000001m);
+            Assert.AreEqual(6.000012m, buildingArea);
         }
+        decimal buildingpoints(decimal Xa, decimal Ya, decimal Xb, decimal Yb, decimal Xc, decimal Yc)
+        {
+            decimal area = Math.Abs((Xa * (Yb - Yc) + Xb * (Yc - Ya) + Xc * (Ya - Yb)) / 2);
+            return area;
+        }
+
     }
 }
