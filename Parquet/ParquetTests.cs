d9e8883e878880588390fce5af4669486ab0a947
ionescuradu <ionescu_radu@ymail.com>
2017-05-18 18:33:38 +0300

    Calculate parquet for a room 10 by 10 with upper rounding

--- Parquet/ParquetTests.cs
+++ Parquet/ParquetTests.cs
@@ -4,11 +4,18 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace Parquet
 {
     [TestClass]
-    public class UnitTest1
+    public class ParquetTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void RoomTenByTen()
         {
+            decimal totalParquet = parquet(10, 10, 2, 3);
+            Assert.AreEqual(20, totalParquet);
+        }
+
+        decimal parquet(decimal roomDimensionOne, decimal roomDimensionTwo, decimal parquetDimensionOne, decimal parquetDimensionTwo)
+        {
+            return Math.Ceiling( roomDimensionOne * roomDimensionTwo * 1.15m / parquetDimensionOne / parquetDimensionTwo );
         }
     }
 }
