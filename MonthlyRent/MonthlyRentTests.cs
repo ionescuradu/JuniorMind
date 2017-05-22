aaa5025ea08591302715ac1596faf3d2fdae41c9
ionescuradu <ionescu_radu@ymail.com>
2017-05-22 19:48:35 +0300

    Calculate rent for medium periods delays

--- MonthlyRent/MonthlyRentTests.cs
+++ MonthlyRent/MonthlyRentTests.cs
@@ -12,9 +12,16 @@ namespace MonthlyRent
             Assert.AreEqual(102, CalculateMonthlyRateDelays(100, 1));
         }
 
+        [TestMethod]
+        public void MonthlyRentForMediumPeriods()
+        {
+            Assert.AreEqual(160, CalculateMonthlyRateDelays(100, 12));
+
+        }
+
         decimal CalculateMonthlyRateDelays(decimal monthlyRent, int delayedDays)
         {
-            decimal shortPeriodPenalities = monthlyRent * delayedDays * 0.02m;
+            decimal shortPeriodPenalities = delayedDays > 11 ? (monthlyRent * delayedDays * 0.05m) : (monthlyRent * delayedDays * 0.02m);
             return monthlyRent + shortPeriodPenalities;
         }
     }
