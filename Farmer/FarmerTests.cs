75c18edb172b96fc957b1ffd1b7d60b381458fed
ionescuradu <ionescu_radu@ymail.com>
2017-05-22 18:38:27 +0300

    Returning the positiv solution for the second grade equation (where it returns 0 it means that there are no viable answers to the problem)

--- Farmer/FarmerTests.cs
+++ Farmer/FarmerTests.cs
@@ -12,10 +12,32 @@ namespace Farmer
             Assert.AreEqual(770, CalculateFarmersLand(1, 230, -770000));
         }
 
-        decimal CalculateFarmersLand(decimal parameter1, decimal parameter2, decimal parameter3)
+        double CalculateFarmersLand(double parameter1, double parameter2, double parameter3)
         {
-            decimal solutionOne = 770;
-            return solutionOne;
+            return GradeTwoEcuationSolutions(parameter1, parameter2, parameter3);
+        }
+
+        private static double GradeTwoEcuationSolutions(double parameter1, double parameter2, double parameter3)
+        {
+            double determiner = parameter2 * parameter2 - 4 * parameter1 * parameter3;
+            if (determiner < 0)
+            {
+                return 0;// Here it should say that the problem doesnt have any real solutions
+            }
+            else
+            {
+            }
+                double solutionOne = (-parameter2 + Math.Sqrt(determiner)) / 2;
+                double solutionTwo = (-parameter2 - Math.Sqrt(determiner)) / 2;
+                if (solutionOne > 0)
+                {
+                    return solutionOne;
+                }
+                else if (solutionTwo > 0)
+                    {
+                        return solutionTwo;
+                    }
+                    return 0; // Here it should say that the problem doesnt have any real solutions
         }
     }
 }
