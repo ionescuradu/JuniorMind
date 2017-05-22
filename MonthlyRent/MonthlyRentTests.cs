acd9748936d0505e7b425bdb7e47bb16dec0b5ce
ionescuradu <ionescu_radu@ymail.com>
2017-05-22 20:22:30 +0300

    Add penalities array

--- MonthlyRent/MonthlyRentTests.cs
+++ MonthlyRent/MonthlyRentTests.cs
@@ -28,12 +28,19 @@ namespace MonthlyRent
 
         decimal CalculateMonthlyRateDelays(decimal monthlyRent, int delayedDays)
         {
-            decimal PeriodPenalities = monthlyRent * delayedDays * 0.02m;
-            if (IsLongPeriod(delayedDays))
-                PeriodPenalities = monthlyRent * delayedDays * 0.1m;
-            else if (IsMediumPeriod(delayedDays))
-                PeriodPenalities = monthlyRent * delayedDays * 0.05m;
+            decimal[] penalities = { 0.02m, 0.05m, 0.1m };
+            decimal PeriodPenalities = GetPeriodPenalities(monthlyRent, delayedDays,penalities);
             return monthlyRent + PeriodPenalities;
+        }
+
+        private decimal GetPeriodPenalities(decimal monthlyRent, int delayedDays, decimal[] penalities)
+        {
+            decimal PeriodPenalities = monthlyRent * delayedDays * penalities[0];
+            if (IsLongPeriod(delayedDays))
+                PeriodPenalities = monthlyRent * delayedDays * penalities[2];
+            else if (IsMediumPeriod(delayedDays))
+                PeriodPenalities = monthlyRent * delayedDays * penalities[1];
+            return PeriodPenalities;
         }
 
         private bool IsLongPeriod(int delayedDays)
