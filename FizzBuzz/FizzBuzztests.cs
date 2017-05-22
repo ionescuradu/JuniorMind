e0ee2bfa9d75c0a6dc6ac085ac6c1bc06a07556c
ionescuradu <ionescu_radu@ymail.com>
2017-05-22 19:25:22 +0300

    Introducing string with outcomes textes

--- FizzBuzz/FizzBuzztests.cs
+++ FizzBuzz/FizzBuzztests.cs
@@ -18,15 +18,28 @@ namespace FizzBuzz
             Assert.AreEqual("Buzz",CalculateNumberDivision(5));
         }
 
+        [TestMethod]
+        public void TestForNumberFifteen()
+        {
+            Assert.AreEqual("FizzBuzz", CalculateNumberDivision(15));
+        }
+
         string CalculateNumberDivision(decimal givenNumber)
+        {
+            string[] textes = { "FizzBuzz", "Fizz", "Buzz"};
+            string outcome = CalculateNumberMultiple(givenNumber, textes);
+            return outcome;
+        }
+
+        private static string CalculateNumberMultiple(decimal givenNumber, string[] textes )
         {
             string outcome = "";
             if (givenNumber % 3 == 0)
                 if (givenNumber % 5 == 0)
-                    outcome = "FizzBuzz";
-                else outcome = "Fizz";
+                    return textes[0];
+                else return textes[1];
             else if (givenNumber % 5 == 0)
-                outcome = "Buzz";
+                return textes[2];
             return outcome;
         }
     }
