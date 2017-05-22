556cc33025e7ab7cb9034a374709b9a61bc8ed60
ionescuradu <ionescu_radu@ymail.com>
2017-05-22 19:05:49 +0300

    Added test for a number multiple of 5

--- FizzBuzz/FizzBuzztests.cs
+++ FizzBuzz/FizzBuzztests.cs
@@ -9,12 +9,24 @@ namespace FizzBuzz
         [TestMethod]
         public void TestForNumberThree()
         {
-            Assert.AreEqual("FIZZ", CalculateNumberDivision(3));
+            Assert.AreEqual("Fizz", CalculateNumberDivision(3));
+        }
+
+        [TestMethod]
+        public void TestForNumberFive()
+        {
+            Assert.AreEqual("Buzz",CalculateNumberDivision(5));
         }
 
         string CalculateNumberDivision(decimal givenNumber)
         {
-            string outcome = "FIZZ";
+            string outcome = "";
+            if (givenNumber % 3 == 0)
+                if (givenNumber % 5 == 0)
+                    outcome = "FizzBuzz";
+                else outcome = "Fizz";
+            else if (givenNumber % 5 == 0)
+                outcome = "Buzz";
             return outcome;
         }
     }
