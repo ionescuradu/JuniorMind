f611d836dfa7053bf16e22903987a129b3fc0095
ionescuradu <ionescu_radu@ymail.com>
2017-05-23 18:46:09 +0300

    Added test for conversion of number 8

--- RomanDigits/RomanDigitsTests.cs
+++ RomanDigits/RomanDigitsTests.cs
@@ -12,9 +12,45 @@ namespace RomanDigits
             Assert.AreEqual("V", CalculateRomanNumber(5));
         }
 
+        [TestMethod]
+        public void TestForOneDigitNumberTestTwo()
+        {
+            Assert.AreEqual("VIII", CalculateRomanNumber(8));
+        }
+
         string CalculateRomanNumber(int givenNumber)
         {
-            string convertedNumber = "V";
+            string convertedNumber = "";
+            switch (givenNumber)
+            {
+                case 1:
+                    convertedNumber = "I";
+                    break;
+                case 2:
+                    convertedNumber = "II";
+                    break;
+                case 3:
+                    convertedNumber = "III";
+                    break;
+                case 4:
+                    convertedNumber = "IV";
+                    break;
+                case 5:
+                    convertedNumber = "V";
+                    break;
+                case 6:
+                    convertedNumber = "VI";
+                    break;
+                case 7:
+                    convertedNumber = "VII";
+                    break;
+                case 8:
+                    convertedNumber = "VIII";
+                    break;
+                case 9:
+                    convertedNumber = "IX";
+                    break;
+            }
             return convertedNumber;
         }
     }
