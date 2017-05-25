ea4e5799adaca3d585c7b9e6de37f6455d3999e2
ionescuradu <ionescu_radu@ymail.com>
2017-05-25 18:32:44 +0300

    Add test for a square of 2 X 2

--- ChessBoard/ChessBoardTests.cs
+++ ChessBoard/ChessBoardTests.cs
@@ -4,11 +4,18 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace ChessBoard
 {
     [TestClass]
-    public class UnitTest1
+    public class ChessBoardTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestForFourSquareDimensionOfChessBoard()
         {
+            Assert.AreEqual(5, CalculateNumberOfSquaresOnChessBoard(2));
+        }
+
+        int CalculateNumberOfSquaresOnChessBoard(int chessDimension)
+        {
+            int numberOfSquares = chessDimension * chessDimension + 1;
+            return numberOfSquares;
         }
     }
 }
