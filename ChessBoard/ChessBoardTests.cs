dc182fe0e7841cca8c54a9c2537b09c661413bc0
ionescuradu <ionescu_radu@ymail.com>
2017-05-27 08:20:57 +0300

    Added test for 4 square chess board, but the first test is now incorrect because (2-3) pow 2 = 1

--- ChessBoard/ChessBoardTests.cs
+++ ChessBoard/ChessBoardTests.cs
@@ -18,11 +18,19 @@ namespace ChessBoard
             Assert.AreEqual(14, CalculateNumberOfSquaresOnChessBoard(3));
         }
 
+        [TestMethod]
+        public void TestForFourSquareDimensionOfChessBoard()
+        {
+            Assert.AreEqual(30, CalculateNumberOfSquaresOnChessBoard(4));
+
+        }
+
         int CalculateNumberOfSquaresOnChessBoard(int chessDimension)
         {
             int numberOfTwoByTwoSquare = Convert.ToInt32( Math.Pow(chessDimension - 1, 2));
             int numberOfThreeByThreeSquare = Convert.ToInt32(Math.Pow(chessDimension - 2, 2));
-            int numberOfSquares = chessDimension * chessDimension + numberOfTwoByTwoSquare + numberOfThreeByThreeSquare;
+            int numberOfFourByFourSquare = Convert.ToInt32(Math.Pow(chessDimension - 3, 2));
+            int numberOfSquares = chessDimension * chessDimension + numberOfTwoByTwoSquare + numberOfThreeByThreeSquare + numberOfFourByFourSquare;
             return numberOfSquares;
         }
     }
