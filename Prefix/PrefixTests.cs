f3f09df9f7604c23d1f4c96b02c409a19dc343ad
ionescuradu <ionescu_radu@ymail.com>
2017-05-27 10:53:17 +0300

    adding a IF for the cases in which the strings have different lenghts

--- Prefix/PrefixTests.cs
+++ Prefix/PrefixTests.cs
@@ -24,10 +24,20 @@ namespace Prefix
             Assert.AreEqual("aaa", CalculateCommonPrefix("aaabc", "aaacb"));
         }
 
+        [TestMethod]
+        public void FourLettersCommonPrefix()
+        {
+            Assert.AreEqual("abcd", CalculateCommonPrefix("abcde", "abcd"));
+        }
+
         string CalculateCommonPrefix (string firstString, string secondString)
         {
+            string longestString;
+            if (firstString.Length >= secondString.Length)
+                longestString = secondString;
+            else longestString = firstString;
             string commonPrefix = "";
-            for ( int i = 0; i < firstString.Length; i++)
+            for ( int i = 0; i < longestString.Length; i++)
             {
                 if (firstString[i] == secondString[i])
                     commonPrefix += firstString[i].ToString();
