4b7aace2cc651509de5296ee8c98f776adc90cd7
ionescuradu <ionescu_radu@ymail.com>
2017-05-27 11:17:26 +0300

    added a string with all the letters from the alphabet

---
+++ Panagram/PanagramTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Panagram
+{
+    [TestClass]
+    public class PanagramTests
+    {
+        [TestMethod]
+        public void FirstSentence()
+        {
+            Assert.AreEqual(false, CalculateIfWordPanagram("The quick brown fox"));
+        }
+
+        bool CalculateIfWordPanagram(string inputSentence)
+        {
+            string allLetters = "abcdefghijklmnopqrstuvwxyz";
+            bool isPanagram = false;
+            return isPanagram;
+        }
+
+    }
+}
