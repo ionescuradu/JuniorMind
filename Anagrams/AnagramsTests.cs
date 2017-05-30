ca6485fe47ac4d4131ae7d943e6adfca2477c13b
ionescuradu <ionescu_radu@ymail.com>
2017-05-30 17:55:55 +0300

    Added test for 4 letter word

--- Anagrams/AnagramsTests.cs
+++ Anagrams/AnagramsTests.cs
@@ -12,10 +12,26 @@ namespace Anagrams
             Assert.AreEqual(6, CalculateNumberOfAnagrams(3));
         }
 
+        [TestMethod]
+        public void FourLetterWord()
+        {
+            Assert.AreEqual(24, CalculateNumberOfAnagrams(4));
+        }
+
         decimal CalculateNumberOfAnagrams(int wordLenght)
         {
-            decimal nrOfAnagrams = 6;
+            decimal nrOfAnagrams = numberOfPermutation(wordLenght);
             return nrOfAnagrams;
+        }
+
+        private decimal numberOfPermutation(int wordLenght)
+        {
+            decimal result = 1;
+            for (int i = 1; i <= wordLenght; i++)
+            {
+                result = result * i;
+            }
+            return result;
         }
     }
 }
