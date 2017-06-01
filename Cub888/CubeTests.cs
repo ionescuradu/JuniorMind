20315dafc77cf79dc65e524311c677a2a2dbb0b5
ionescuradu <ionescu_radu@ymail.com>
2017-06-01 14:08:52 +0300

    Modified the solution and deleted some variables for tests

--- Cub888/CubeTests.cs
+++ Cub888/CubeTests.cs
@@ -21,21 +21,21 @@ namespace Cub888
         double CalculateSpecificCubes (int indexNumber)
         {
             string sufix = "888";
-            double[] arrayNumbers = new double [200];
+            double[] arrayNumbers = new double [2000000];
             int index = 1;
             string concatenateNumber = "";
             for (int i = 1; i < double.MaxValue ; i++)
             {
                 concatenateNumber = Convert.ToString(i) + sufix;
                 double radical = 1d / 3;
-                //double convertedtodouble = Convert.ToDouble(concatenateNumber);
-                //double convertedtodoublecuputere = Math.Pow(Convert.ToDouble(concatenateNumber), radical);
-                //double numarConvertit = Convert.ToInt16(Math.Pow(Convert.ToDouble(concatenateNumber), radical));
-                if (( Math.Pow(Convert.ToDouble(concatenateNumber), radical) == Convert.ToInt32(Math.Pow(Convert.ToDouble(concatenateNumber), radical))))
+                if (Math.Abs(Convert.ToInt32(Math.Pow(Convert.ToDouble(concatenateNumber), radical)) - Math.Pow(Convert.ToDouble(concatenateNumber), radical)) < 0.000000000001d)
                 {
-                    arrayNumbers[index] = i;
+                    arrayNumbers[index] = Convert.ToInt32(Math.Pow(Convert.ToDouble(concatenateNumber), radical));
                     index += 1;
                 }
+                
+                //if (( Math.Pow(Convert.ToDouble(concatenateNumber), radical) == Convert.ToInt32(Math.Pow(Convert.ToDouble(concatenateNumber), radical))))
+                
             }
             return arrayNumbers[indexNumber];
         }
