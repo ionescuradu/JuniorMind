e69cdb80f0170ccf4dbe47dccd5fd810eee31788
ionescuradu <ionescu_radu@ymail.com>
2017-06-01 13:27:50 +0300

    Added part of solution

--- Cub888/CubeTests.cs
+++ Cub888/CubeTests.cs
@@ -12,10 +12,32 @@ namespace Cub888
             Assert.AreEqual(192, CalculateSpecificCubes(1));
         }
 
-        double CalculateSpecificCubes (double indexNumber)
+        [TestMethod]
+        public void SecondNumber()
         {
-            double[] arrayNumbers = {192d};
-            return arrayNumbers[0];
+            Assert.AreEqual(442, CalculateSpecificCubes(2));
+        }
+
+        double CalculateSpecificCubes (int indexNumber)
+        {
+            string sufix = "888";
+            double[] arrayNumbers = new double [200];
+            int index = 1;
+            string concatenateNumber = "";
+            for (int i = 1; i < double.MaxValue ; i++)
+            {
+                concatenateNumber = Convert.ToString(i) + sufix;
+                double radical = 1d / 3;
+                //double convertedtodouble = Convert.ToDouble(concatenateNumber);
+                //double convertedtodoublecuputere = Math.Pow(Convert.ToDouble(concatenateNumber), radical);
+                //double numarConvertit = Convert.ToInt16(Math.Pow(Convert.ToDouble(concatenateNumber), radical));
+                if (( Math.Pow(Convert.ToDouble(concatenateNumber), radical) == Convert.ToInt32(Math.Pow(Convert.ToDouble(concatenateNumber), radical))))
+                {
+                    arrayNumbers[index] = i;
+                    index += 1;
+                }
+            }
+            return arrayNumbers[indexNumber];
         }
     }
 }
