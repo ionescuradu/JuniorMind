18080eda264188a46d0980d353ce58c22e5fe359
ionescuradu <ionescu_radu@ymail.com>
2017-06-10 12:49:19 +0300

    added test for 1 char long password lower case

---
+++ Password/PasswordTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Password
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual("a", GeneratePassword(1, Options.LowerCases));
+        }
+        enum Options
+        {
+            LowerCases,
+            UpperCaseesAndNumber,
+            NumbersAndHowMany,
+            SimbolsAndNumber,
+            WithoutSpeacialChars,
+            WithoutAmbiguuChars
+        }
+        string GeneratePassword(int passwordLenght, Options style)
+        {
+            string password = "a";
+            return password;
+        }
+    }
+}
