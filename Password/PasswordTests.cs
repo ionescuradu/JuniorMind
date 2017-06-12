33f5b3f59e0098f79312523c3b69acf48304d177
ionescuradu <ionescu_radu@ymail.com>
2017-06-12 17:24:29 +0300

    added a new test and modifying few parameters

--- Password/PasswordTests.cs
+++ Password/PasswordTests.cs
@@ -12,6 +12,17 @@ namespace Password
             Assert.AreEqual(1, GeneratePassword(1, Options.LowerCases));
         }
 
+        [TestMethod]
+        public void MultiLowerCase()
+        {
+            Assert.AreEqual(3, GeneratePassword(3, Options.LowerCases));
+        }
+
+        [TestMethod]
+        {
+            Assert.AreEqual(10, GeneratePassword(10, Options.UpperCaseesAndNumber));
+        }
+
         enum Options
         {
             LowerCases,
@@ -23,7 +34,7 @@ namespace Password
         }
         int GeneratePassword(int passwordLenght, Options style)
         {
-            string password = "a";
+            char[] password = GenerateRandom('a', 'z', passwordLenght);
             int counter = 0;
             for (int i = 0; i < password.Length; i++)
             {
@@ -31,6 +42,16 @@ namespace Password
                     counter += 1;
             }
             return counter;
+        }
+
+        private char[] GenerateRandom(int start, int end, int passwordLenght)
+        {
+            char[] generatedNumbers = new char[passwordLenght];
+            Random random = new Random();
+            for (int i = 0; i < passwordLenght; i++)
+            {
+            }
+            return generatedNumbers;
         }
     }
 }
