7e4c146d79ab9c66500bc32f144c4f001273cc55
ionescuradu <ionescu_radu@ymail.com>
2017-06-18 11:38:36 +0300

    added verifcation for password

--- Password/PasswordTests.cs
+++ Password/PasswordTests.cs
@@ -9,7 +9,7 @@ namespace Password
         [TestMethod]
         public void OneLowerLetter()
         {
-            Assert.AreEqual("", GeneratePassword(new Options { passwordLenght = 4, upperCases = 1, numbers = 1, simbols = 1, withoutAmbigous = false, withoutSimilar = false}));
+            Assert.AreEqual("Password is Correct", GeneratePassword(new Options { passwordLenght = 4, upperCases = 1, numbers = 1, simbols = 1, withoutAmbigous = false, withoutSimilar = false}));
         }
 
         struct Options
@@ -39,19 +39,95 @@ namespace Password
                 randomSimilar = GenerateRandomNumber(1, remainingCharacters);
             int remainingLowerCase = rest - randomSimilar;
             finalPassword = GenerateRandomString(options.upperCases, 'A', 'Z', similarCharacters) + GenerateRandomString(remainingLowerCase, 'a', 'z', similarCharacters) + GenerateRandomString(options.numbers, '0', '9', similarCharacters) + GenerateSimbols(options.passwordLenght, options.simbols, simbolsVector) + GenerateSimilar(options.passwordLenght, similarCharacters, randomSimilar) + GenerateAmbigous(options.passwordLenght, ambiguousCharacters, randomAmbigous);
+            char[] randomize = RandomizePassword(finalPassword);
+            finalPassword = new string(randomize);
+            var indexSmall = 0;
+            var indexUpper = 0;
+            var indexNumber = 0;
+            var indexAmbigous = false;
+            var indexSimilar = false;
+            var indexSimbols = 0;
+            var indexAmbigous2 = 0;
+            var indexSimilar2 = 0;
+            bool correctPassword = false;
+            for (int i = 0; i < finalPassword.Length; i++)
+            {
+                if (finalPassword[i] >= 'a' && finalPassword[i] <= 'z')
+                {
+                    indexSmall += 1;
+                }
+                if (finalPassword[i] >= 'A' && finalPassword[i] <= 'Z')
+                {
+                    indexUpper += 1;
+                }
+                if (finalPassword[i] >= '0' && finalPassword[i] <= '9')
+                {
+                    indexNumber += 1;
+                }
+                for (int j = 0; j < ambiguousCharacters.Length; j++)
+                {
+                    if (finalPassword[i] == ambiguousCharacters[j])
+                    {
+                        indexAmbigous = true;
+                        indexAmbigous2 += 1;
+                    }
+                }
+                for (int k = 0; k < similarCharacters.Length; k++)
+                {
+                    if (finalPassword[i] == similarCharacters[k])
+                    {
+                        indexSimilar = true;
+                        indexSimilar2 += 1;
+                    }
+
+                }
+                for (int m = 0; m < simbolsVector.Length; m++)
+                {
+                    if (finalPassword[i] == simbolsVector[m])
+                    {
+                        indexSimbols += 1;
+                    }
+                }
+            }
+            for (int i = 0; i < finalPassword.Length; i++)
+            {
+                if (finalPassword[i] == 'o' || finalPassword[i] == 'i')
+                {
+                    indexSmall -= 1;
+                }
+                if (finalPassword[i] == 'O' || finalPassword[i] == 'L')
+                {
+                    indexUpper -= 1;
+                }
+                if (finalPassword[i] == '0' || finalPassword[i] == '1')
+                {
+                    indexNumber -= 1;
+                }
+            }
+            if (indexSimbols == options.simbols && indexSimilar == options.withoutSimilar && indexNumber == options.numbers && indexAmbigous == options.withoutAmbigous && indexUpper == options.upperCases && options.passwordLenght == (indexAmbigous2 + indexSimilar2 + indexSmall + indexUpper + indexSimbols + indexNumber))
+                correctPassword = true;
+            if (correctPassword == true)
+                return "Password is Correct";
+            else
+                return "Password is Incorrect";
+        }
+
+        private char[] RandomizePassword(string finalPassword)
+        {
             char[] randomize = finalPassword.ToCharArray();
             int lenght = randomize.Length - 1;
-            while (lenght >= 0 )
+            while (lenght >= 0)
             {
                 var random = GenerateRandomNumber(0, lenght + 1);
                 var value = randomize[random];
                 randomize[random] = randomize[lenght];
                 randomize[lenght] = value;
-                lenght--; 
+                lenght--;
             }
-            finalPassword = new string(randomize);
-            return finalPassword;
+
+            return randomize;
         }
+
         private string GenerateSimbols(int passwordLenght, int simbols, string simbolsVector)
         {
             string random = "";
@@ -105,10 +181,9 @@ namespace Password
             }
             return random;
         }
-
+        Random random = new Random();
         private int GenerateRandomNumber(int start, int end)
         {
-            Random random = new Random();
             return random.Next(start, end);
         }
     }
