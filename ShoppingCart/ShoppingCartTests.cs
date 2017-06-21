4e1ba3e2dd148f3f31984f4ace5e9679bb6bbd02
ionescuradu <ionescu_radu@ymail.com>
2017-06-21 17:58:19 +0300

    added first test for method and calculate the sum of the shopping cart

--- ShoppingCart/ShoppingCartTests.cs
+++ ShoppingCart/ShoppingCartTests.cs
@@ -7,8 +7,32 @@ namespace ShoppingCart
     public class ShoppingCartTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void FirstCartTest()
         {
+            var shoppingCart = new Product[2] { new Product("castraveti", 10f), new Product("rosii", 20f) };
+            Assert.AreEqual(30, CalculateTotal(shoppingCart));
+        }
+
+        public struct Product
+        {
+            public string productName;
+            public float price;
+
+            public Product(string productName, float price)
+            {
+                this.productName = productName;
+                this.price = price;
+            }
+        }
+
+        float CalculateTotal(Product[] shoppingCart)
+        {
+            float sum = 0;
+            for (int i = 0; i < shoppingCart.Length; i++)
+            {
+                sum += shoppingCart[i].price  ;
+            }
+            return sum;
         }
     }
 }
