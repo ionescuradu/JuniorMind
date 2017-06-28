0e00883dc231a140a76dce743989fa39f265fbf6
ionescuradu <ionescu_radu@ymail.com>
2017-06-28 19:33:22 +0300

    added first test for one biker tota distance

--- Bicycle/bicycleTests.cs
+++ Bicycle/bicycleTests.cs
@@ -4,11 +4,41 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace Bicycle
 {
     [TestClass]
-    public class UnitTest1
+    public class bicycleTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void OneCyclist()
         {
+            var data = new Data[2] { new Data("Radu", 1, 10, 75), new Data("Radu", 2, 15, 75) };
+            Assert.AreEqual(5887.5m, Distance(data));
+        }
+
+        public struct Data
+        {
+            public string name;
+            public int secondCount;
+            public int rotationCount;
+            public int wheelDiam;
+
+            public Data(string name, int secondCount, int rotationCount, int wheelDiam)
+            {
+                this.name = name;
+                this.secondCount = secondCount;
+                this.rotationCount = rotationCount;
+                this.wheelDiam = wheelDiam;
+            }
+        }
+
+        decimal Distance(Data[] data)
+        {
+            var rotations = 0;
+            decimal distance = 0;
+            for (int i = 0; i < data.Length; i++)
+            {
+                rotations += data[i].rotationCount;   
+            }
+            distance = rotations * data[0].wheelDiam * 3.14m;
+            return distance;
         }
     }
 }
