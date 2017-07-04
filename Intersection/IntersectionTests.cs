a847c50f4d6adc4cbffec503a4da98d86f8ac3c4
ionescuradu <ionescu_radu@ymail.com>
2017-07-04 17:59:59 +0300

    added first test without intersection

--- Intersection/IntersectionTests.cs
+++ Intersection/IntersectionTests.cs
@@ -4,11 +4,25 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace Intersection
 {
     [TestClass]
-    public class UnitTest1
+    public class IntersectionTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void IntersectionFirstTest()
         {
+            Assert.AreEqual(false, Intersection(new string[4] { "stanga", "jos", "dreapta", "jos" }));
+        }
+
+        bool Intersection(string[] directions)
+        {
+            bool verdict = true;
+            for (int i = 0; i < directions.Length - 3; i++)
+            {
+                if (directions[i] == directions[i + 1] || directions[i] == directions[i + 2] || directions[i] == directions[i + 3] || directions[i + 1] == directions[i + 2] || directions[i + 1] == directions[i + 3] || directions[i + 2] == directions[i + 3])
+                {
+                    verdict = false;
+                }
+            }
+            return verdict;
         }
     }
 }
