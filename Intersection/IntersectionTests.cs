e9adff0eefaad595aa1328c18bbda4c431fc3ac9
ionescuradu <ionescu_radu@ymail.com>
2017-07-06 17:25:46 +0300

    modified the method for intersection

--- Intersection/IntersectionTests.cs
+++ Intersection/IntersectionTests.cs
@@ -9,13 +9,29 @@ namespace Intersection
         [TestMethod]
         public void IntersectionFirstTest()
         {
-            Assert.AreEqual("000", Intersection(new Ways[4] { Ways.left, Ways.down, Ways.right, Ways.down }));
+            Assert.AreEqual(new Coordonates { x = 0, y = 0 }, IntersectionPoint(new Coordonates {x = 0, y =0 }, 1 ,new Ways[4] { Ways.left, Ways.down, Ways.right, Ways.down }));
         }
 
         [TestMethod]
         public void IntersectionSecondTest()
         {
-            Assert.AreEqual("125", Intersection(new Ways[6] { Ways.left, Ways.down, Ways.right, Ways.down, Ways.left, Ways.up }));
+            Assert.AreEqual(new Coordonates { x = -1, y = -1 }, IntersectionPoint(new Coordonates { x = 0, y = 0 }, 1 , new Ways[6] { Ways.left, Ways.down, Ways.right, Ways.down, Ways.left, Ways.up }));
+        }
+
+        [TestMethod]
+        public void IntersectionThirdTest()
+        {
+            Assert.AreEqual(new Coordonates { x = 1, y = 2 }, IntersectionPoint(new Coordonates { x = 0, y = 0 }, 1 , new Ways[11] { Ways.right, Ways.up, Ways.up, Ways.up, Ways.up, Ways.up, Ways.left, Ways.down, Ways.down, Ways.down, Ways.right }));
+        }
+        public struct Coordonates
+        {
+            public int x;
+            public int y;
+            public Coordonates(int x, int y)
+            {
+                this.x = x;
+                this.y = y;
+            }
         }
 
         enum Ways
@@ -25,25 +41,44 @@ namespace Intersection
             right,
             left
         }
-
-        string Intersection(Ways[] directions)
+        Coordonates IntersectionPoint(Coordonates startingPoint, int segmentLenght, Ways[] direction)
         {
-            int[] firstIntersection = new int[3];
-            string intersectionPoint = "";
-            for (int i = 0; i < directions.Length - 3; i++)
+            var points = new Coordonates[direction.Length + 1];
+            points[0].x = startingPoint.x;
+            points[0].y = startingPoint.y;
+            var meetingPoint = new Coordonates(0, 0);
+            for (int i = 0; i < direction.Length; i++)
             {
-                if (directions[i] != directions[i + 1] && directions[i] != directions[i + 2] && directions[i] != directions[i + 3] && directions[i + 1] != directions[i + 2] && directions[i + 1] != directions[i + 3] && directions[i + 2] != directions[i + 3])
+                if (direction[i] == Ways.up)
                 {
-                    firstIntersection[0] = i - 1;
-                    firstIntersection[1] = i;
-                    firstIntersection[2] = i + 3;
+                    points[i + 1].y = points[i].y + segmentLenght;
+                    points[i + 1].x = points[i].x;
+                }
+                if (direction[i] == Ways.down)
+                {
+                    points[i + 1].y = points[i].y - segmentLenght;
+                    points[i + 1].x = points[i].x;
+                }
+                if (direction[i] == Ways.left)
+                {
+                    points[i + 1].y = points[i].y;
+                    points[i + 1].x = points[i].x - segmentLenght;
+                }
+                if (direction[i] == Ways.right)
+                {
+                    points[i + 1].y = points[i].y;
+                    points[i + 1].x = points[i].x + segmentLenght;
+                }
+                for (int j = 0; j < i; j++)
+                {
+                    if (points[j].x == points[i + 1].x && points[j].y == points[i + 1].y)
+                    {
+                        meetingPoint.x = points[i + 1].x;
+                        meetingPoint.y = points[i + 1].y;
+                    }
                 }
             }
-            for (int i = 0; i < firstIntersection.Length; i++)
-            {
-                intersectionPoint += firstIntersection[i];
-            }
-            return intersectionPoint;
+            return meetingPoint;
         }
     }
 }
