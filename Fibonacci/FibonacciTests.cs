aa448951ba5de1bfd67c175446bc3a4334f94af3
ionescuradu <ionescu_radu@ymail.com>
2017-07-07 17:09:54 +0300

    added first Fobonacci test

--- Fibonacci/FibonacciTests.cs
+++ Fibonacci/FibonacciTests.cs
@@ -4,11 +4,21 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace Fibonacci
 {
     [TestClass]
-    public class UnitTest1
+    public class FibonacciTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void FibonacciFirst()
         {
+            Assert.AreEqual(0, Fibonacci(0));
+        }
+
+        int Fibonacci(int givenNumber)
+        {
+            if (givenNumber < 2)
+            {
+                return givenNumber;
+            }
+            else return Fibonacci(givenNumber - 1) + Fibonacci(givenNumber - 2);
         }
     }
 }
