979bce349780eaad30abda5d9bff76ea8ef73230
ionescuradu <ionescu_radu@ymail.com>
2017-07-07 17:21:12 +0300

    Added first reverse string test

--- StringReversing/StringReversingTests.cs
+++ StringReversing/StringReversingTests.cs
@@ -4,11 +4,21 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace StringReversing
 {
     [TestClass]
-    public class UnitTest1
+    public class StringReversingTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void ReverseStringFirst()
         {
+            Assert.AreEqual("udar", ReverseString("radu"));
+        }
+
+        string ReverseString(string givenString)
+        {
+            if (givenString.Length > 0)
+            {
+                return givenString[givenString.Length - 1] + ReverseString(givenString.Substring(0, givenString.Length - 1));
+            }
+            else return givenString;
         }
     }
 }
