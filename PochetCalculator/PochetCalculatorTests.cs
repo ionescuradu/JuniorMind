9466a8232af975497713088e706f9682743d33e9
ionescuradu <ionescu_radu@ymail.com>
2017-07-10 21:25:27 +0300

    added 3rd test for pochet calculator

--- PochetCalculator/PochetCalculatorTests.cs
+++ PochetCalculator/PochetCalculatorTests.cs
@@ -12,58 +12,77 @@ namespace PochetCalculator
             Assert.AreEqual(12, PochetCalculator(new string[3] { "*", "3", "4" }));
         }
 
+        [TestMethod]
+        public void CalculatorSecondTest()
+        {
+            Assert.AreEqual(4, PochetCalculator(new string[5] { "*", "+", "1", "1", "2" }));
+        }
+
+        [TestMethod] 
+        public void CalculatorThirdTest()
+        {
+            Assert.AreEqual(1564.75m, PochetCalculator(new string[11] { "+", "/", "*", "+", "56", "46", "46", "3", "-", "1", "0.25" }));
+        }
+
         decimal PochetCalculator(string[] givenArray)
         {
-            string[] buffer = new string[givenArray.Length];
             if (givenArray.Length != 1)
             {
                 for (int i = givenArray.Length - 1; i >= 0; i--)
                 {
                     if (givenArray[i] == "*" || givenArray[i] == "/" || givenArray[i] == "+" || givenArray[i] == "-")
                     {
-                        string switchCase = givenArray[i];
-                        switch (switchCase)
+                        switch (givenArray[i])
                         {
                             case "+":
                                 givenArray[i + 1] = Convert.ToString(Convert.ToDecimal(givenArray[i + 1]) + Convert.ToDecimal(givenArray[i + 2]));
                                 givenArray[i + 2] = "";
                                 givenArray[i] = "";
-                                break;
+                                givenArray = ResizingArray(givenArray);
+                                return PochetCalculator(givenArray);
                             case "-":
                                 givenArray[i + 1] = Convert.ToString(Convert.ToDecimal(givenArray[i + 1]) - Convert.ToDecimal(givenArray[i + 2]));
                                 givenArray[i + 2] = "";
                                 givenArray[i] = "";
-                                break;
+                                givenArray = ResizingArray(givenArray);
+                                return PochetCalculator(givenArray);
                             case "/":
                                 givenArray[i + 1] = Convert.ToString(Convert.ToDecimal(givenArray[i + 1]) / Convert.ToDecimal(givenArray[i + 2]));
                                 givenArray[i + 2] = "";
                                 givenArray[i] = "";
-                                break;
+                                givenArray = ResizingArray(givenArray);
+                                return PochetCalculator(givenArray);
                             case "*":
                                 givenArray[i + 1] = Convert.ToString(Convert.ToDecimal(givenArray[i + 1]) * Convert.ToDecimal(givenArray[i + 2]));
                                 givenArray[i + 2] = "";
                                 givenArray[i] = "";
-                                break;
+                                givenArray = ResizingArray(givenArray);
+                                return PochetCalculator(givenArray);
                         }
-                    }
 
-                }
-                var index = 0;
-                for (int i = 0; i < givenArray.Length; i++)
-                {
-                    if (givenArray[i] == "")
-                    {
-                        index += 1;
-                    }
-                    else
-                    {
-                        buffer[i - index] = givenArray[i];
                     }
                 }
-                Array.Resize(ref buffer, buffer.Length - index);
-                return PochetCalculator(buffer);
+                return PochetCalculator(givenArray);
             }
             else return Convert.ToDecimal(givenArray[0]);
+        }
+
+        private static string[] ResizingArray(string[] givenArray)
+        {
+            var index = 0;
+            for (int j = 0; j < givenArray.Length; j++)
+            {
+                if (givenArray[j] == "")
+                {
+                    index += 1;
+                }
+                else
+                {
+                    givenArray[j - index] = givenArray[j];
+                }
+            }
+            Array.Resize(ref givenArray, givenArray.Length - index);
+            return givenArray;
         }
     }
 }
