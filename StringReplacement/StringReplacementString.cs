537ec244f22ad6df096fd4cd22cbadcf732cc4e9
ionescuradu <ionescu_radu@ymail.com>
2017-07-10 20:10:07 +0300

    added method for string concatenation and first test

--- StringReplacement/StringReplacementString.cs
+++ StringReplacement/StringReplacementString.cs
@@ -4,11 +4,37 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace StringReplacement
 {
     [TestClass]
-    public class UnitTest1
+    public class StringReplacementString
     {
         [TestMethod]
-        public void TestMethod1()
+        public void StringReplacementFirstTest()
         {
+            Assert.AreEqual("RIONESCUDU", ConcatenateTwoStrings("RADU", "IONESCU", 1));
+        }
+
+        string ConcatenateTwoStrings(string first, string second, int insertionPoint)
+        {
+            string result = "";
+            var bufferString = first;
+            for (int i = 0; i < insertionPoint; i++)
+            {
+                result += first[i];
+            }
+            int counter = - 1;
+            Concatenate(ref result, second, counter);
+            result += first.Substring(insertionPoint + 1, first.Length - insertionPoint - 1);
+            return result;
+        }
+
+        private string Concatenate( ref string result, string second, int counter)
+        {
+            if (counter < second.Length - 1)
+            {
+                counter += 1;
+                result += second[counter];
+                return Concatenate(ref result, second, counter);
+            }
+            else return result;
         }
     }
 }
