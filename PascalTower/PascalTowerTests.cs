ddf6dd821af4efd59c6f9f8cf767f25c800e2a2b
ionescuradu <ionescu_radu@ymail.com>
2017-07-11 16:58:13 +0300

    added first and second test for Pascal triangle

--- PascalTower/PascalTowerTests.cs
+++ PascalTower/PascalTowerTests.cs
@@ -4,11 +4,35 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace PascalTower
 {
     [TestClass]
-    public class UnitTest1
+    public class PascalTowerTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void PascalFirstTest()
         {
+            CollectionAssert.AreEqual(new int[1] { 1 }, Triangle(1));
+        }
+
+        [TestMethod]
+        public void PascalSecondTest()
+        {
+            CollectionAssert.AreEqual(new int[2] { 1 , 1 }, Triangle(2));
+        }
+
+        int[] Triangle(int row)
+        {
+            int[] triangleLevel = new int[row];
+            for (int i = 0; i < row; i++)
+            {
+                triangleLevel[i] = Pascal(row, i);
+            }
+            return triangleLevel;
+        }
+
+        private int Pascal(int row, int element)
+        {
+            if (row <= 2 || element == 1 || row == element)
+                return 1;
+            return Pascal(row - 1, element - 1) + Pascal(row - 1, element);
         }
     }
 }
