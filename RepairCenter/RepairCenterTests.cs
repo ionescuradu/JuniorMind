a2fed24ea571c45bc55c0e217d4d8ca91e6e5baa
ionescuradu <ionescu_radu@ymail.com>
2017-07-24 16:36:28 +0300

    added first test for Repair center

--- RepairCenter/RepairCenterTests.cs
+++ RepairCenter/RepairCenterTests.cs
@@ -4,11 +4,56 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace RepairCenter
 {
     [TestClass]
-    public class UnitTest1
+    public class RepairCenterTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void RepairCenterFirst()
         {
+            CollectionAssert.AreEqual(new priority[3] { priority.High, priority.Medium, priority.Low }, sortingArray(new priority[3] { priority.Low, priority.High, priority.Medium }));
+        }
+
+        enum priority
+        {
+            High,
+            Medium,
+            Low
+        }
+
+        priority[] sortingArray(priority[] givenArray)
+        {
+            var index = 0; //se poate extrage functia de sortare pe low, medium, high !!!!!!!!!!!
+            var auxiliar = priority.High;
+            for (int i = 0; i < givenArray.Length; i++)
+            {
+                if (givenArray[i] == priority.High)
+                {
+                    auxiliar = givenArray[index];
+                    givenArray[index] = priority.High;
+                    givenArray[i] = auxiliar;
+                    index += 1;
+                }
+            }
+            for (int i = 0; i < givenArray.Length; i++)
+            {
+                if (givenArray[i] == priority.Medium)
+                {
+                    auxiliar = givenArray[index];
+                    givenArray[index] = priority.Medium;
+                    givenArray[i] = auxiliar;
+                    index += 1;
+                }
+            }
+            for (int i = 0; i < givenArray.Length; i++)
+            {
+                if (givenArray[i] == priority.Low)
+                {
+                    auxiliar = givenArray[index];
+                    givenArray[index] = priority.Low;
+                    givenArray[i] = auxiliar;
+                    index += 1;
+                }
+            }
+            return givenArray;
         }
     }
 }
