6628c4d6e575204dd738eda97b5296ce890c7761
ionescuradu <ionescu_radu@ymail.com>
2017-08-31 19:03:21 +0300

    added new function in struct and removing all unwanted methods and lines

--- ClassBookTests/ClassBookTests.cs
+++ ClassBookTests/ClassBookTests.cs
@@ -19,8 +19,8 @@ namespace ClassBookTests
               new Topic("Math", new int[2] { 10, 7 }),
               new Topic("Physics", new int[2] { 10, 9 })
             };
-            var andreea = new Student("Andreea", andreeaGrades, 0m);
-            var radu = new Student("Radu", raduGrades, 0m);
+            var andreea = new Student("Andreea", andreeaGrades);
+            var radu = new Student("Radu", raduGrades);
             CollectionAssert.AreEqual(new Student[2] { andreea, radu }, ClassBookOrderingByName(new Student[] { radu, andreea }));
         }
 
@@ -42,10 +42,9 @@ namespace ClassBookTests
               new Topic("Chemistry", new int[2] { 10, 8 }),
               new Topic("Math", new int[2] { 10, 6 })
             };
-
-            var andreea = new Student("Andreea", andreeaGrades, 0m);
-            var radu = new Student("Radu", raduGrades, 0m);
-            var antonia = new Student("Antonia", antoniaGrades, 0m);
+            var andreea = new Student("Andreea", andreeaGrades);
+            var radu = new Student("Radu", raduGrades);
+            var antonia = new Student("Antonia", antoniaGrades);
             CollectionAssert.AreEqual(new Student[3] { andreea, antonia, radu }, ClassBookOrderingByName(new Student[] { radu, andreea, antonia }));
         }
 
@@ -62,11 +61,9 @@ namespace ClassBookTests
               new Topic("Math", new int[2] { 10, 7 }),
               new Topic("Physics", new int[2] { 10, 9 })
             };
-            var andreeaAverage = new Student("Andreea", andreeaGrades, 9.75m);
-            var raduAverage = new Student("Radu", raduGrades, 9m);
-            var andreea = new Student("Andreea", andreeaGrades, 0m);
-            var radu = new Student("Radu", raduGrades, 0m);
-            CollectionAssert.AreEqual(new Student[2] { andreeaAverage, raduAverage }, ClassBookOverallAverage(new Student[] { radu, andreea}));
+            var andreea = new Student("Andreea", andreeaGrades);
+            var radu = new Student("Radu", raduGrades);
+            CollectionAssert.AreEqual(new Student[2] { andreea, radu }, ClassBookOverallAverage(new Student[] { radu, andreea}));
         }
 
         [TestMethod]
@@ -87,13 +84,10 @@ namespace ClassBookTests
               new Topic("Chemistry", new int[2] { 10, 8 }),
               new Topic("Math", new int[2] { 10, 6 })
             };
-            var andreeaAverage = new Student("Andreea", andreeaGrades, 9.75m);
-            var raduAverage = new Student("Radu", raduGrades, 9m);
-            var antoniaAverage = new Student("Antonia", antoniaGrades, 8.5m);
-            var andreea = new Student("Andreea", andreeaGrades, 0m);
-            var radu = new Student("Radu", raduGrades, 0m);
-            var antonia = new Student("Antonia", antoniaGrades, 0m);
-            CollectionAssert.AreEqual(new Student[3] { andreeaAverage, raduAverage, antoniaAverage }, ClassBookOverallAverage(new Student[] { radu, andreea, antonia }));
+            var andreea = new Student("Andreea", andreeaGrades);
+            var radu = new Student("Radu", raduGrades);
+            var antonia = new Student("Antonia", antoniaGrades);
+            CollectionAssert.AreEqual(new Student[3] { andreea, radu, antonia }, ClassBookOverallAverage(new Student[] { radu, andreea, antonia }));
         }
 
         [TestMethod]
@@ -109,11 +103,9 @@ namespace ClassBookTests
               new Topic("Math", new int[2] { 10, 7 }),
               new Topic("Physics", new int[2] { 10, 9 })
             };
-            var andreeaAverage = new Student("Andreea", andreeaGrades, 9.75m);
-            var raduAverage = new Student("Radu", raduGrades, 9m);
-            var andreea = new Student("Andreea", andreeaGrades, 0m);
-            var radu = new Student("Radu", raduGrades, 0m);
-            CollectionAssert.AreEqual(new Student[1] { andreeaAverage}, ClassBookSpecificAverage(new Student[] { radu, andreea }, 9.75m));
+            var andreea = new Student("Andreea", andreeaGrades);
+            var radu = new Student("Radu", raduGrades);
+            CollectionAssert.AreEqual(new Student[1] { andreea }, ClassBookSpecificAverage(new Student[] { radu, andreea }, 9.75m));
         }
 
         [TestMethod]
@@ -134,13 +126,10 @@ namespace ClassBookTests
               new Topic("Chemistry", new int[2] { 10, 8 }),
               new Topic("Math", new int[2] { 10, 8 })
             };
-            var andreeaAverage = new Student("Andreea", andreeaGrades, 9.75m);
-            var raduAverage = new Student("Radu", raduGrades, 9m);
-            var antoniaAverage = new Student("Antonia", antoniaGrades, 9m);
-            var andreea = new Student("Andreea", andreeaGrades, 0m);
-            var radu = new Student("Radu", raduGrades, 0m);
-            var antonia = new Student("Antonia", antoniaGrades, 0m);
-            CollectionAssert.AreEqual(new Student[2] {raduAverage, antoniaAverage }, ClassBookSpecificAverage(new Student[] { radu, andreea, antonia }, 9m));
+            var andreea = new Student("Andreea", andreeaGrades);
+            var radu = new Student("Radu", raduGrades);
+            var antonia = new Student("Antonia", antoniaGrades);
+            CollectionAssert.AreEqual(new Student[2] { radu, antonia }, ClassBookSpecificAverage(new Student[] { radu, andreea, antonia }, 9m));
         }
 
         [TestMethod]
@@ -156,10 +145,8 @@ namespace ClassBookTests
               new Topic("Math", new int[2] { 10, 7 }),
               new Topic("Physics", new int[2] { 10, 9 })
             };
-            var andreeaAverage = new Student("Andreea", andreeaGrades, 9.75m);
-            var raduAverage = new Student("Radu", raduGrades, 9m);
-            var andreea = new Student("Andreea", andreeaGrades, 0m);
-            var radu = new Student("Radu", raduGrades, 0m);
+            var andreea = new Student("Andreea", andreeaGrades);
+            var radu = new Student("Radu", raduGrades);
             CollectionAssert.AreEqual(new Student[1] { andreea }, ClassBookBestStudets(new Student[] { radu, andreea }));
         }
 
@@ -181,12 +168,9 @@ namespace ClassBookTests
               new Topic("Chemistry", new int[2] { 10, 10 }),
               new Topic("Math", new int[2] { 10, 8 })
             };
-            var andreeaAverage = new Student("Andreea", andreeaGrades, 9.75m);
-            var raduAverage = new Student("Radu", raduGrades, 9m);
-            var antoniaAverage = new Student("Antonia", antoniaGrades, 9m);
-            var andreea = new Student("Andreea", andreeaGrades, 0m);
-            var radu = new Student("Radu", raduGrades, 0m);
-            var antonia = new Student("Antonia", antoniaGrades, 0m);
+            var andreea = new Student("Andreea", andreeaGrades);
+            var radu = new Student("Radu", raduGrades);
+            var antonia = new Student("Antonia", antoniaGrades);
             CollectionAssert.AreEqual(new Student[2] { andreea, antonia }, ClassBookBestStudets(new Student[] { radu, andreea, antonia }));
         }
 
@@ -233,13 +217,11 @@ namespace ClassBookTests
         {
             public Topic[] pupil;
             public string name;
-            public decimal generalAverage;
 
-            public Student(string name, Topic[] pupil, decimal generalAverage)
+            public Student(string name, Topic[] pupil)
             {
                 this.pupil = pupil;
                 this.name = name;
-                this.generalAverage = generalAverage;
             }
 
             public int CountGradesOf10PerStudent()
@@ -292,18 +274,16 @@ namespace ClassBookTests
 
         Student[] ClassBookOverallAverage(Student[] givenList) 
         {
-            AverageCalculation(givenList);
             SelectionSorting(givenList);
             return givenList;
         }
 
         Student[] ClassBookSpecificAverage(Student[] givenList, decimal givenAverage)
         {
-            AverageCalculation(givenList);
             var index = 0;
             for (int i = 0; i < givenList.Length; i++)
             {
-                if (givenList[i].generalAverage == givenAverage)
+                if (givenList[i].TotalSubjectAverage() == givenAverage)
                 {
                     index += 1;
                 }
@@ -312,7 +292,7 @@ namespace ClassBookTests
             var aux = 0;
             for (int i = 0; i < givenList.Length; i++)
             {
-                if (givenList[i].generalAverage == givenAverage)
+                if (givenList[i].TotalSubjectAverage() == givenAverage)
                 {
                     specificStudents[aux] = givenList[i];
                     aux += 1;
@@ -352,40 +332,18 @@ namespace ClassBookTests
             return bestStudents;
         }
 
-        private static void AverageCalculation(Student[] givenList)
-        {
-            for (int i = 0; i < givenList.Length; i++) 
-            {
-                var average = 0m;
-                var indexAverage = 0;
-                for (int j = 0; j < givenList[i].pupil.Length; j++) 
-                {
-                    var sum = 0m;
-                    var indexSum = 0;
-                    for (int k = 0; k < givenList[i].pupil[j].grades.Length; k++) 
-                    {
-                        sum += givenList[i].pupil[j].grades[k];
-                        indexSum += 1;
-                    }
-                    average += sum / indexSum; 
-                    indexAverage += 1;
-                }
-                givenList[i].generalAverage = average / indexAverage; 
-            }
-        }
-
         private static void SelectionSorting(Student[] givenList)
         {
             var index = 0;
             var indexFinal = 0;
-            var max = givenList[0].generalAverage;
+            var max = givenList[0].TotalSubjectAverage();
             while (indexFinal != givenList.Length - 1)
             {
                 for (int i = indexFinal + 1; i < givenList.Length; i++)
                 {
-                    if (givenList[i].generalAverage > max)
+                    if (givenList[i].TotalSubjectAverage() > max)
                     {
-                        max = givenList[i].generalAverage;
+                        max = givenList[i].TotalSubjectAverage();
                         index = i;
                     }
                 }
