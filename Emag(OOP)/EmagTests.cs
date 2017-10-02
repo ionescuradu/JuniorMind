6b0905c1f1b09aeb501e98f07fd6ce7bf299e46f
ionescuradu <ionescu_radu@ymail.com>
2017-10-02 19:17:15 +0300

    Added new method for subtracting a phone

--- Emag(OOP)/EmagTests.cs
+++ Emag(OOP)/EmagTests.cs
@@ -14,13 +14,13 @@ namespace Emag_OOP_
             var specThirdPhone = new Spec(1, 4.5m, 16);
             var specFourthPhone = new Spec(4, 5.5m, 64);
 
-            var phone1 = new Phone(specFirstPhone, Promotion.New);
-            var phone2 = new Phone(specSecondPhone, Promotion.Promotion);
-            var phone3 = new Phone(specThirdPhone, Promotion.New);
-            var phone4 = new Phone(specFourthPhone, Promotion.Resealed);
+            var phone1 = new Phone(specFirstPhone, Promotion.New, 20, PhoneNames.LGG6);
+            var phone2 = new Phone(specSecondPhone, Promotion.Promotion, 50, PhoneNames.IPHONE7);
+            var phone3 = new Phone(specThirdPhone, Promotion.New, 25, PhoneNames.S7EDGE);
+            var phone4 = new Phone(specFourthPhone, Promotion.Resealed, 15, PhoneNames.S8PLUS);
             PhoneList phonelist = new PhoneList(new Phone[4] { phone1, phone2, phone3, phone4 });
             Spec spec = new Spec(2, 5, 0);
-            var givenCriterias = new Phone(spec, Promotion.Promotion);
+            var givenCriterias = new Phone(spec, Promotion.Promotion, 0, 0);
 
             CollectionAssert.AreEqual(new Phone[1] { phone2 }, phonelist.FindPhones(givenCriterias));
         }
@@ -33,13 +33,13 @@ namespace Emag_OOP_
             var specThirdPhone = new Spec(1, 4.5m, 16);
             var specFourthPhone = new Spec(4, 5.5m, 64);
 
-            var phone1 = new Phone(specFirstPhone, Promotion.New);
-            var phone2 = new Phone(specSecondPhone, Promotion.Promotion);
-            var phone3 = new Phone(specThirdPhone, Promotion.New);
-            var phone4 = new Phone(specFourthPhone, Promotion.Resealed);
+            var phone1 = new Phone(specFirstPhone, Promotion.New, 20, PhoneNames.LGG6);
+            var phone2 = new Phone(specSecondPhone, Promotion.Promotion, 50, PhoneNames.IPHONE7);
+            var phone3 = new Phone(specThirdPhone, Promotion.New, 25, PhoneNames.S7EDGE);
+            var phone4 = new Phone(specFourthPhone, Promotion.Resealed, 15, PhoneNames.S8PLUS);
             PhoneList phonelist = new PhoneList(new Phone[4] { phone1, phone2, phone3, phone4 });
             Spec spec = new Spec(2, 0, 0);
-            var givenCriterias = new Phone(spec, 0);
+            var givenCriterias = new Phone(spec, 0, 0, 0);
 
             CollectionAssert.AreEqual(new Phone[3] { phone1, phone2, phone4 }, phonelist.FindPhones(givenCriterias));
         }
@@ -52,15 +52,35 @@ namespace Emag_OOP_
             var specThirdPhone = new Spec(1, 4.5m, 16);
             var specFourthPhone = new Spec(4, 5.5m, 64);
 
-            var phone1 = new Phone(specFirstPhone, Promotion.New);
-            var phone2 = new Phone(specSecondPhone, Promotion.Promotion);
-            var phone3 = new Phone(specThirdPhone, Promotion.New);
-            var phone4 = new Phone(specFourthPhone, Promotion.Resealed);
+            var phone1 = new Phone(specFirstPhone, Promotion.New, 20, PhoneNames.LGG6);
+            var phone2 = new Phone(specSecondPhone, Promotion.Promotion, 50, PhoneNames.IPHONE7);
+            var phone3 = new Phone(specThirdPhone, Promotion.New, 25, PhoneNames.S7EDGE);
+            var phone4 = new Phone(specFourthPhone, Promotion.Resealed, 15, PhoneNames.S8PLUS);
             PhoneList phonelist = new PhoneList(new Phone[4] { phone1, phone2, phone3, phone4 });
             Spec spec = new Spec(1, 4.5m, 0);
-            var givenCriterias = new Phone(spec, 0);
+            var givenCriterias = new Phone(spec, 0, 0, 0);
 
             CollectionAssert.AreEqual(new Phone[4] { phone1, phone2, phone3, phone4 }, phonelist.FindPhones(givenCriterias));
+        }
+
+        [TestMethod]
+        public void EmagTest4()
+        {
+            var specFirstPhone = new Spec(2, 5.5m, 32);
+            var specSecondPhone = new Spec(2, 5m, 64);
+            var specThirdPhone = new Spec(1, 4.5m, 16);
+            var specFourthPhone = new Spec(4, 5.5m, 64);
+
+            var phone1 = new Phone(specFirstPhone, Promotion.New, 20, PhoneNames.LGG6);
+            var phone2 = new Phone(specSecondPhone, Promotion.Promotion, 50, PhoneNames.IPHONE7);
+            var phone3 = new Phone(specThirdPhone, Promotion.New, 25, PhoneNames.S7EDGE);
+            var phone4 = new Phone(specFourthPhone, Promotion.Resealed, 15, PhoneNames.S8PLUS);
+            var phone2Sell = new Phone(specSecondPhone, Promotion.Promotion, 30, PhoneNames.IPHONE7);
+            PhoneList phonelist = new PhoneList(new Phone[4] { phone1, phone2, phone3, phone4 });
+            PhoneNames phoneName = PhoneNames.IPHONE7;
+            int sellNumber = 20;
+
+            CollectionAssert.AreEqual(new Phone[4] { phone1, phone2Sell, phone3, phone4 }, phonelist.SellPhone(phoneName, sellNumber));
         }
     }
 }
--- Emag(OOP)/Phone.cs
+++ Emag(OOP)/Phone.cs
@@ -15,20 +15,45 @@ namespace Emag_OOP_
         Resealed,
         Preorder
     }
+
+    public enum PhoneNames
+    {
+        IPHONE7,
+        S7EDGE,
+        S8PLUS,
+        LGG6
+    }
+
     public class Phone
     {
         private Spec spec;
         private Promotion promotion;
+        private int stoc;
+        private PhoneNames name;
 
-        public Phone(Spec spec, Promotion promotion)
+        public Phone(Spec spec, Promotion promotion, int stoc, PhoneNames name)
         {
             this.spec = spec;
             this.promotion = promotion;
+            this.stoc = stoc;
+            this.name = name;
         }
 
         public bool Compare(Phone another)
         {
-            return (spec.CompareRam(another.spec) && spec.CompareInternalMemory(another.spec) && spec.CompareDisplaySize(another.spec) && (promotion == another.promotion || another.promotion == 0));
+            return (spec.CompareRam(another.spec) 
+                && spec.CompareInternalMemory(another.spec) 
+                && spec.CompareDisplaySize(another.spec) 
+                && (promotion == another.promotion || another.promotion == 0));
+        }
+        public bool FindPhone(PhoneNames phoneName)
+        {
+            return (name == phoneName);
+        }
+
+        public void PhoneSubtract(int sellNumber)
+        {
+            stoc -= sellNumber;
         }
     }
 
