a8a5261e87328bf46b32d0a0c1a76ab7d51dac41
ionescuradu <ionescu_radu@ymail.com>
2017-10-03 19:44:43 +0300

    added second test for stoc variations

--- Emag(OOP)/EmagTests.cs
+++ Emag(OOP)/EmagTests.cs
@@ -81,5 +81,24 @@ namespace Emag_OOP_
 
             Assert.AreEqual(30, phone2.PhoneSubtract(sellNumber));
         }
+
+        [TestMethod]
+        public void EmagTest5()
+        {
+            var specFirstPhone = new Spec(2, 5.5m, 32);
+            var specSecondPhone = new Spec(2, 5m, 64);
+            var specThirdPhone = new Spec(1, 4.5m, 16);
+            var specFourthPhone = new Spec(4, 5.5m, 64);
+
+            var phone1 = new Phone(specFirstPhone, Promotion.New, 20, PhoneNames.LGG6);
+            var phone2 = new Phone(specSecondPhone, Promotion.Promotion, 50, PhoneNames.IPHONE7);
+            var phone3 = new Phone(specThirdPhone, Promotion.New, 25, PhoneNames.S7EDGE);
+            var phone4 = new Phone(specFourthPhone, Promotion.Resealed, 15, PhoneNames.S8PLUS);
+            var phone2Sell = new Phone(specSecondPhone, Promotion.Promotion, 30, PhoneNames.IPHONE7);
+            PhoneList phonelist = new PhoneList(new Phone[4] { phone1, phone2, phone3, phone4 });
+            int sellNumber = 10;
+
+            Assert.AreEqual(10, phone1.PhoneSubtract(sellNumber));
+        }
     }
 }
