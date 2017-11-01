cd32d74b1cfba06d551153a4ebfb4cceee2bb5d6
ionescuradu <ionescu_radu@ymail.com>
2017-11-01 19:41:02 +0200

    first test for adding an element

---
+++ IListT_Tests/IListT_Tests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace IListT_Tests
+{
+    [TestClass]
+    public class IListT_Tests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            List<int> givenList = new List<int>(8);
+            givenList.Add(2);
+            var compareList = new List<int>(8) { 2 };
+            CollectionAssert.AreEqual(compareList, givenList);
+        }
+    }
+}
