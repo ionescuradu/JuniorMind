f1a2b5fa4b08cca2dcbf5db9e90549506ad9c532
ionescuradu <ionescu_radu@ymail.com>
2017-11-21 20:39:09 +0200

    modified GetEnumerator method

--- LinkedListTests/List.cs
+++ LinkedListTests/List.cs
@@ -59,7 +59,7 @@ namespace LinkedListTests
 
         public IEnumerator<T> GetEnumerator()
         {
-            new VectorEnumerator<T>(root);
+            return new VectorEnumerator<T>(root);
         }
 
         IEnumerator IEnumerable.GetEnumerator()
