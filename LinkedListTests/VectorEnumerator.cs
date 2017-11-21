dbd11d463b211db7e19a0dc8d479d13bf034bc6b
ionescuradu <ionescu_radu@ymail.com>
2017-11-21 19:04:46 +0200

    added VectorEnumerator method

---
+++ LinkedListTests/VectorEnumerator.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace LinkedListTests
+{
+    internal class VectorEnumerator<T> : IEnumerator<T>
+    {
+        private Node<T> root;
+
+        public VectorEnumerator(Node<T> root)
+        {
+            this.root = root;
+        }
+
+        public T Current => throw new NotImplementedException();
+
+        object IEnumerator.Current => throw new NotImplementedException();
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool MoveNext()
+        {
+            return (root.Next != null);
+        }
+
+        public void Reset()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
