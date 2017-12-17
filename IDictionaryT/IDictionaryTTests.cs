805de25195e26209df3978e7b23d3e4a33c370c0
ionescuradu <ionescu_radu@ymail.com>
2017-12-17 11:25:09 +0200

    new tests for IDictionary

---
+++ IDictionaryT/IDictionaryTTests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace IDictionaryT
+{
+    [TestClass]
+    public class IDictionaryTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var bookCase = new IDictionary <T>();
+        }
+    }
+}
---
+++ IDictionaryT/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("IDictionaryT")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("IDictionaryT")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("18fc5050-5600-41ee-9d7a-4dbf6e67accb")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
