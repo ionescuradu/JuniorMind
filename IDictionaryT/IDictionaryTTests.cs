eaf7f112a61278b7c8ef52eb08e5a129b684c067
ionescuradu <ionescu_radu@ymail.com>
2018-01-05 18:29:50 +0200

    added second test for Add method

--- IDictionaryT/IDictionaryTTests.cs
+++ IDictionaryT/IDictionaryTTests.cs
@@ -16,11 +16,29 @@ namespace IDictionaryT
             {
                 dictionary[i] = new List<Entry<int, int>>();   
             }
-            var newEntry = new Entry<int, int>(1, 100);
+            var newEntry1 = new Entry<int, int>(1, 100);
             var key = 1;
             var insertBucket = key.GetHashCode() % dictionary.Length;
-            dictionary[insertBucket].Add(newEntry);
-            Assert.AreEqual(true, dictionary[insertBucket].Contains(newEntry));
+            dictionary[insertBucket].Add(newEntry1);
+            Assert.AreEqual(true, dictionary[insertBucket].Contains(newEntry1));
+        }
+
+        [TestMethod]
+        public void DictionaryAdd2()
+        {
+            var dictionary = new List<Entry<int, int>>[3];
+            for (int i = 0; i < dictionary.Length; i++)
+            {
+                dictionary[i] = new List<Entry<int, int>>();
+            }
+            var newEntry1 = new Entry<int, int>(1, 100);
+            var newEntry2 = new Entry<int, int>(2, 102);
+            var key = 1;
+            var insertBucket = key.GetHashCode() % dictionary.Length;
+            dictionary[insertBucket].Add(newEntry1);
+            Assert.AreEqual(false, dictionary[insertBucket].Contains(newEntry2));
         }
     }
+
+
 }
