5388fd2830854e40bcc3b3913dd57db22a9e8bca
ionescuradu <ionescu_radu@ymail.com>
2018-01-05 18:35:09 +0200

    Added third test for Add and Contains methods

--- IDictionaryT/IDictionaryTTests.cs
+++ IDictionaryT/IDictionaryTTests.cs
@@ -38,6 +38,26 @@ namespace IDictionaryT
             dictionary[insertBucket].Add(newEntry1);
             Assert.AreEqual(false, dictionary[insertBucket].Contains(newEntry2));
         }
+
+        [TestMethod]
+        public void DictionaryAdd3()
+        {
+            var dictionary = new List<Entry<int, int>>[3];
+            for (int i = 0; i < dictionary.Length; i++)
+            {
+                dictionary[i] = new List<Entry<int, int>>();
+            }
+            var newEntry1 = new Entry<int, int>(1, 100);
+            var newEntry2 = new Entry<int, int>(2, 102);
+            var key = 1;
+            var key2 = 2;
+            var insertBucket = key.GetHashCode() % dictionary.Length;
+            var insertBucket2 = key2.GetHashCode() % dictionary.Length;
+            dictionary[insertBucket].Add(newEntry1);
+            dictionary[insertBucket2].Add(newEntry2);
+            Assert.AreEqual(false, dictionary[insertBucket].Contains(newEntry2));
+        }
+
     }
 
 
