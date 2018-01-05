be9bf1906b591d211e3aeab72082990ce42dbf83
ionescuradu <ionescu_radu@ymail.com>
2018-01-05 20:52:12 +0200

    modified test for 3rd Add method

--- IDictionaryT/IDictionaryTTests.cs
+++ IDictionaryT/IDictionaryTTests.cs
@@ -34,37 +34,23 @@ namespace IDictionaryT
         [TestMethod]
         public void DictionaryAdd3()
         {
-            var dictionary = new List<Entry<int, int>>[3];
-            for (int i = 0; i < dictionary.Length; i++)
-            {
-                dictionary[i] = new List<Entry<int, int>>();
-            }
-            var newEntry1 = new Entry<int, int>(1, 100);
-            var newEntry2 = new Entry<int, int>(2, 102);
-            var key = 1;
-            var key2 = 2;
-            var insertBucket = key.GetHashCode() % dictionary.Length;
-            var insertBucket2 = key2.GetHashCode() % dictionary.Length;
-            dictionary[insertBucket].Add(newEntry1);
-            dictionary[insertBucket2].Add(newEntry2);
-            Assert.AreEqual(false, dictionary[insertBucket].Contains(newEntry2));
+            var initialCapacity = 3;
+            var dictionary = new Dictionary<int, int>(initialCapacity);
+            dictionary.ResetEntries();
+            var entry1 = new KeyValuePair<int, int>(1, 100);
+            var entry2 = new KeyValuePair<int, int>(2, 102);
+            var entry3 = new KeyValuePair<int, int>(1, 103);
+            dictionary.Add(entry1);
+            dictionary.Add(entry2);
+            dictionary.Add(entry3);
+            Assert.AreEqual(true, dictionary.Contains(entry3));
         }
 
         [TestMethod]
         [ExpectedException(typeof(ArgumentException))]
         public void DictionaryAddException1()
         {
-            var dictionary = new List<Entry<int, int>>[3];
-            for (int i = 0; i < dictionary.Length; i++)
-            {
-                dictionary[i] = new List<Entry<int, int>>();
-            }
-            var newEntry1 = new Entry<int, int>(1, 100);
-            var newEntry2 = new Entry<int, int>(1, 102);
-            var key = 1;
-            var insertBucket = key.GetHashCode() % dictionary.Length;
-            dictionary[insertBucket].Add(newEntry1);
-            dictionary[insertBucket].Add(newEntry2);
+
         }
 
     }
