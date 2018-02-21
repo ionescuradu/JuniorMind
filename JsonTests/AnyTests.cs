9bcb7fbd61f1996d2b663f75f9781c7f06dc1340
ionescuradu <ionescu_radu@ymail.com>
2018-02-21 18:45:55 +0200

    added first test for Any class

---
+++ JsonTests/AnyTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace JsonTests
+{
+    [TestClass]
+    public class AnyTests
+    {
+        [TestMethod]
+        public void AnyTest1()
+        {
+            var x = new Any("radu");
+            var (match, remainng) = x.Match("axy");
+            Assert.IsTrue(match.Success);
+            Assert.AreEqual(remainng, "xy");
+        }
+    }
+}
