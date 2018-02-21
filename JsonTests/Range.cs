0b298e993ae65c77e6967a0b09b6bf6d3c8ee721
ionescuradu <ionescu_radu@ymail.com>
2018-02-21 14:17:27 +0200

    added new class Range, new test unit for Range class and first 2 tests

---
+++ JsonTests/Range.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JsonTests
+{
+    class Range : Pattern
+    {
+        private char startChar;
+        private char endChar;
+
+        public Range(char startChar, char endChar)
+        {
+            this.startChar = startChar;
+            this.endChar = endChar;
+        }
+
+        public (Match, string) Match(string input)
+        {
+            if (input.Equals(""))
+            {
+                return (new NoText(input[0].ToString()), input);
+            }
+            if (input[0] >= startChar && input[0] <=endChar)
+            {
+                return (new SuccessMatch(input[0].ToString()), input.Substring(1));
+            }
+            return (new NoMatch(input[0].ToString(), input[0]), input);
+        }
+    }
+}
---
+++ JsonTests/RangeTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace JsonTests
+{
+    [TestClass]
+    public class RangeTests
+    {
+        [TestMethod]
+        public void RangeTest1()
+        {
+            var x = new Range('a', 'z');
+            var (match, remainng) = x.Match("x");
+            Assert.IsTrue(match.Success);
+            Assert.AreEqual(remainng, "");
+        }
+
+        [TestMethod]
+        public void RangeTest2()
+        {
+            var x = new Range('a', 'z');
+            var (match, remainng) = x.Match("xa");
+            Assert.IsTrue(match.Success);
+            Assert.AreEqual(remainng, "a");
+        }
+    }
+}
