f617f38499da142e3a9a410ac76cb5fd7e69e13c
ionescuradu <ionescu_radu@ymail.com>
2018-02-21 18:37:30 +0200

    added method for AnyMatch

--- JsonTests/Any.cs
+++ JsonTests/Any.cs
@@ -6,7 +6,25 @@ using System.Threading.Tasks;
 
 namespace JsonTests
 {
-    class Class1
+    class Any : Pattern
     {
+        private string givenText;
+
+        public Any(string givenText)
+        {
+            this.givenText = givenText;
+        }
+
+        public (Match, string) Match(string input)
+        {
+            for (int i = 0; i < givenText.Length; i++)
+            {
+                if (input[0] == givenText[i])
+                {
+                    return (new SuccessMatch(input[0].ToString()), input.Substring(1));
+                }
+            }
+            return (new NoMatch(input[0].ToString(), input[0]), input);
+        }
     }
 }
