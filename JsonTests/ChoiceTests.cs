8213afcff09dd92da725e6bac13585747ec83d66
ionescuradu <ionescu_radu@ymail.com>
2018-02-22 19:06:49 +0200

    added third test for Choice method

--- JsonTests/ChoiceTests.cs
+++ JsonTests/ChoiceTests.cs
@@ -17,5 +17,18 @@ namespace JsonTests
             Assert.IsTrue(match.Success);
             Assert.AreEqual(remaining, "adu");
         }
+
+        [TestMethod]
+        public void SequanceTest2()
+        {
+            string input = "radu";
+            var x = new Range('a', 'z');
+            var y = new Any("mno");
+            var z = new Any("abc");
+            var choices = new Choice(x, y, z);
+            var (match, remaining) = choices.Match(input);
+            Assert.IsTrue(match.Success);
+            Assert.AreEqual(remaining, "du");
+        }
     }
 }
