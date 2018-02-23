f654acb7390b51a9bdb9ced44830612186582bf1
ionescuradu <ionescu_radu@ymail.com>
2018-02-23 15:35:39 +0200

    added second test for OneOrMore

--- JsonTests/OneOrMoreTests.cs
+++ JsonTests/OneOrMoreTests.cs
@@ -14,5 +14,14 @@ namespace JsonTests
             Assert.IsTrue(match.Success);
             Assert.AreEqual(remainng, "adu");
         }
+
+        [TestMethod]
+        public void OneOrMore2()
+        {
+            var x = new OneOrMore(new Character('r'));
+            var (match, remainng) = x.Match("ionescu");
+            Assert.IsFalse(match.Success);
+            Assert.AreEqual(remainng, "ionescu");
+        }
     }
 }
