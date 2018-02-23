2dc6e07fdbbcc99f6f2444bd9bc2cfe8c51fee91
ionescuradu <ionescu_radu@ymail.com>
2018-02-23 14:50:59 +0200

    added new class Optional and new tests for it

---
+++ JsonTests/Optional.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JsonTests
+{
+    class Optional : Pattern
+    {
+        private Pattern pattern;
+
+        public Optional(Pattern pattern)
+        {
+            this.pattern = pattern;
+        }
+
+        public (Match, string) Match(string input)
+        {
+            var (match, remaining) = pattern.Match(input);
+            return (new SuccessMatch(input[input[input.Length - remaining.Length]].ToString()), remaining);
+        }
+    }
+}
---
+++ JsonTests/OptionalTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace JsonTests
+{
+    [TestClass]
+    public class OptionalTests
+    {
+        [TestMethod]
+        public void OptionalTest1()
+        {
+
+            var x = new Optional(new Character('-'));
+            var (match, remaining) = x.Match("-radu");
+            Assert.IsTrue(match.Success);
+            Assert.AreEqual(remaining, "rau");
+        }
+    }
+}
