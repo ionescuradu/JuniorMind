8e8cca233955ea16bbcd9b18b72657c8a56e3acc
ionescuradu <ionescu_radu@ymail.com>
2018-02-23 13:06:01 +0200

    added new class Text, new test for this class

--- JsonTests/Text.cs
+++ JsonTests/Text.cs
@@ -8,11 +8,22 @@ namespace JsonTests
 {
     class Text : Pattern
     {
-        
+        private Character[] chars;
+        private string givenText;
+
+        public Text(string givenText)
+        {
+            this.givenText = givenText;
+            chars = new Character[givenText.Length];
+            for (int i = 0; i < givenText.Length; i++)
+            {
+                chars[i] = new Character(givenText[i]);
+            }
+        }
 
         public (Match, string) Match(string input)
         {
-            throw new NotImplementedException();
+            return new Sequance(chars).Match(input);
         }
     }
 }
---
+++ JsonTests/TextTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace JsonTests
+{
+    [TestClass]
+    public class TextTests
+    {
+        [TestMethod]
+        public void TextTest1()
+        {
+            string input = "raduionescu";
+            var x = new Text("rad");
+            var (match, remaining) = x.Match(input);
+            Assert.IsTrue(match.Success);
+            Assert.AreEqual(remaining, "uionescu");
+        }
+    }
+}
