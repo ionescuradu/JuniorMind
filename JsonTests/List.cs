54e589e7e03ada302dc59a656eb7a80f22518719
ionescuradu <ionescu_radu@ymail.com>
2018-02-24 14:18:43 +0200

    added new class List and new test for it

---
+++ JsonTests/List.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JsonTests
+{
+    class List : Pattern
+    {
+        private Pattern Characters;
+        private Pattern Separator;
+
+        public List(Pattern Characters, Pattern Separator)
+        {
+            this.Characters = Characters;
+            this.Separator = Separator;
+        }
+
+        public (Match, string) Match(string input)
+        {
+            if (input == "")
+            {
+                return (new SuccessMatch(""), "");
+            }
+            return (new NoMatch("", ' '), input);
+        }
+    }
+}
---
+++ JsonTests/ListTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace JsonTests
+{
+    [TestClass]
+    public class ListTests
+    {
+        [TestMethod]
+        public void ListTest1()
+        {
+            string input = "";
+            var x = new List(new Character('r'), new Character(' '));
+            var (match, remaining) = x.Match(input);
+            Assert.IsTrue(match.Success);
+            Assert.AreEqual(remaining, "");
+        }
+    }
+}
