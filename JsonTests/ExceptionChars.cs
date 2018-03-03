31a8ed89aa15581eccc6c39ad1cba9341deefdb0
ionescuradu <ionescu_radu@ymail.com>
2018-03-03 15:47:44 +0200

    added new test unit for ExceptionChars class, and added new method same class

---
+++ JsonTests/ExceptionChars.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JsonTests
+{
+    class ExceptionChars : Pattern
+    {
+        readonly private Choice exceptionChars;
+        readonly private Any hexazecimal;
+
+        public ExceptionChars()
+        {
+            hexazecimal = new Any("0123456789ABCDEFabcdef");
+            exceptionChars = new Choice(
+                new Sequance(new Character((char)92),
+                                  new Character('u'),
+                                  new Character('0'),
+                                  new Character('0'),
+                                  new Character('1'),
+                                  hexazecimal
+                                  ),
+                new Sequance(new Character((char)92),
+                                  new Character('u'),
+                                  new Character('0'),
+                                  new Character('2'),
+                                  new Character('2')
+                                  ),
+                new Sequance(new Character((char)92),
+                                  new Character('u'),
+                                  new Character('0'),
+                                  new Character('5'),
+                                  new Any("Cc")
+                                  )
+                                  );
+        }
+
+        public (Match, string) Match(string input)
+        {
+            if (input == "")
+            {
+                return (new NoText(input), input);
+            }
+            var (match, remaining) = exceptionChars.Match(input);
+            if (match.Success)
+            {
+                return (new NoMatch(input, input[0]), input);
+            }
+            return (new SuccessMatch(input.Substring(0, input.Length - remaining.Length)), remaining);
+        }
+    }
+}
---
+++ JsonTests/ExceptionCharsTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace JsonTests
+{
+    [TestClass]
+    public class ExceptionCharsTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
