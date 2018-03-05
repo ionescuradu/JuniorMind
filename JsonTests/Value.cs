72149eb69c3915acb7b98b3785e92d9e885c26a2
ionescuradu <ionescu_radu@ymail.com>
2018-03-05 13:37:25 +0200

    added new class Value, and first test for this class

---
+++ JsonTests/Value.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JsonTests
+{
+    class Value : Pattern
+    {
+        readonly private Choice givenValue;
+
+        public Value()
+        {
+            givenValue = new Choice(
+                new String(),
+                new Numbers(),
+                new Text("True"),
+                new Text("False"),
+                new Text("null"));
+        }
+
+        public (Match, string) Match(string input)
+        {
+            var (match, remaining) = givenValue.Match(input);
+            if (match.Success)
+            {
+                int foundString = input.Length - remaining.Length;
+                return (new SuccessMatch(input.Substring(0, foundString)), remaining);
+            }
+            return (new NoMatch(input, ' '), remaining);
+        }
+    }
+}
---
+++ JsonTests/ValueTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace JsonTests
+{
+    [TestClass]
+    public class ValueTests
+    {
+        [TestMethod]
+        public void ValueTest1()
+        {
+            var x = new Value();
+            var (match, remaining) = x.Match("");
+            Assert.IsFalse(match.Success);
+            Assert.AreEqual("", remaining);
+        }
+    }
+}
