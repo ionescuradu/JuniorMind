7fbe2f8901f646c246a4aad7c253e806799f3105
ionescuradu <ionescu_radu@ymail.com>
2018-03-07 11:21:10 +0200

    made first console app

---
+++ ConsoleApp1/JsonValidation.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using JsonTests;
+
+namespace JsonValidation
+{
+    class JsonValidation
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Please insert a text to be evaluated");
+                Console.WriteLine("Press any key to exit");
+                Console.ReadKey();
+            }
+            var x = new Json();
+            var (match, remaining) = x.Match(args[0].ToString());
+            if (match.Success && remaining == "")
+            {
+                Console.WriteLine("Text is correct");
+                Console.WriteLine("Press any key to exit");
+                Console.ReadKey();
+            }
+            Console.WriteLine("Text is incorrect");
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
+        }
+    }
+}
