94a2d6c5a50c35568b6cf6406df375cadd68d6b5
ionescuradu <ionescu_radu@ymail.com>
2018-03-14 11:56:47 +0200

    added new class for SocketServer and moved the constructor in this class

--- Socket/Socket.cs
+++ Socket/Socket.cs
@@ -5,17 +5,6 @@ namespace Socket
 {
     public class Socket
     {
-        private readonly AddressFamily addressFamily;
-        private readonly SocketType socketType;
-        private readonly ProtocolType protocolType;
-
-        public Socket(AddressFamily addressFamily, SocketType socketType, ProtocolType protocolType)
-        {
-            this.addressFamily = AddressFamily.InterNetwork;
-            this.socketType = SocketType.Stream;
-            this.protocolType = ProtocolType.IPv4;
-        }
-
         public static void Main()
         {
             
---
+++ Socket/SocketServer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Net.Sockets;
+
+namespace Socket
+{
+    class SocketServer
+    {
+        private readonly AddressFamily addressFamily;
+        private readonly SocketType socketType;
+        private readonly ProtocolType protocolType;
+
+        public SocketServer(AddressFamily addressFamily, SocketType socketType, ProtocolType protocolType)
+        {
+            this.addressFamily = AddressFamily.InterNetwork;
+            this.socketType = SocketType.Stream;
+            this.protocolType = ProtocolType.Tcp;
+        }
+    }
+}
