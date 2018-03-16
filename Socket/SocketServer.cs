c0de237fa8777ebe32eb369e4ca82ac3e5838922
ionescuradu <ionescu_radu@ymail.com>
2018-03-16 09:39:56 +0200

    added new console aplication for SocketServer + 2 examples from Msdn for better understanding of the socket feature

--- Socket/SocketServer.cs
+++ Socket/SocketServer.cs
@@ -1,26 +1,73 @@
 ﻿using System;
 using System.Net;
 using System.Net.Sockets;
+using System.Text;
 
 
-namespace Socket
+namespace SocketServer
 {
-    class SocketServer
+    public class SynchronousSocketServer
     {
-        private readonly AddressFamily addressFamily;
-        private readonly SocketType socketType;
-        private readonly ProtocolType protocolType;
+        public static string data = null;
 
-        public SocketServer(AddressFamily addressFamily, SocketType socketType, ProtocolType protocolType)
+
+        public static void StartServer()
         {
-            this.addressFamily = addressFamily;
-            this.socketType = socketType;
-            this.protocolType = protocolType;
+            byte[] bytes = new Byte[1024];
+            IPHostEntry iPHostEntry = Dns.GetHostEntry(Dns.GetHostName());
+            IPAddress ipAddress = ipHostInfo.AddressList[0];
+            IPEndPoint endPoint = new IPEndPoint(ipAddress, 4500);
+
+            Socket listener = new Socket(ipAddress.AddressFamily,
+            SocketType.Stream, ProtocolType.Tcp);
+
+            try
+            {
+                listener.Bind(endPoint);
+                listener.Listen(10);
+
+                // Start listening for connections.  
+                while (true)
+                {
+                    Console.WriteLine("Waiting for a connection...");
+                    // Program is suspended while waiting for an incoming connection.  
+                    Socket handler = listener.Accept();
+                    data = null;
+
+                    // An incoming connection needs to be processed.  
+                    while (true)
+                    {
+                        bytes = new byte[1024];
+                        int bytesRec = handler.Receive(bytes);
+                        data += Encoding.ASCII.GetString(bytes, 0, bytesRec);
+                        if (data.IndexOf("<EOF>") > -1)
+                        {
+                            break;
+                        }
+                    }
+
+                    // Show the data on the console.  
+                    Console.WriteLine("Text received : {0}", data);
+
+                    // Echo the data back to the client.  
+                    byte[] msg = Encoding.ASCII.GetBytes(data);
+
+                    handler.Send(msg);
+                    handler.Shutdown(SocketShutdown.Both);
+                    handler.Close();
+                }
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
         }
 
-        public void Bind(IPEndPoint ipEndPoint)
+        public static int Main(String[] args)
         {
-            throw new NotImplementedException();
+            StartServer();
+            return 0;
         }
     }
 }
---
+++ SocketServer/SocketServer.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace SocketServer
+{
+    public class SocketServer
+    {
+
+        public static string data;
+
+        public static void StartServer()
+        {
+            byte[] bytes = new Byte[1024];
+            IPHostEntry iPHostEntry = Dns.GetHostEntry(Dns.GetHostName());
+            IPAddress iPAddress = iPHostEntry.AddressList[0];
+            IPEndPoint endPoint = new IPEndPoint(iPAddress, 4500);
+
+            Socket server = new Socket(
+                iPAddress.AddressFamily, 
+                SocketType.Stream, 
+                ProtocolType.Tcp
+                );
+            try
+            {
+                server.Bind(endPoint);
+                server.Listen(1);
+
+                while (true)
+                {
+                    Console.WriteLine("Waiting for a connection");
+                    Socket transmition = server.Accept();
+                    data = null;
+
+                    while (true)
+                    {
+                        bytes = new byte[1024];
+                        int bytesRec = transmition.Receive(bytes);
+                        data += Encoding.ASCII.GetString(bytes, 0, bytesRec);
+                        if (data.IndexOf("<EOF>") > -1)
+                        {
+                            break;
+                        }
+                    }
+
+                    Console.WriteLine("Text received : {0}", data);
+                    byte[] msg = Encoding.ASCII.GetBytes(data);
+                    transmition.Send(msg);
+                    transmition.Shutdown(SocketShutdown.Both);
+                    transmition.Close();
+                }
+            }
+            catch (Exception e)
+            {
+
+                Console.WriteLine(e.ToString());
+            }
+
+
+        }
+
+
+
+        public static void Main(string[] args)
+        {
+            StartServer();
+        }
+    }
+}
