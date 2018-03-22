86ff5d69fab92a8c6aa664955f8ed936a940eb8f
ionescuradu <ionescu_radu@ymail.com>
2018-03-22 09:24:19 +0200

    created new class for parsing the Uri from them the html header, and added first test for it

---
+++ TcpTextVerify/UriParsing.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using JsonTests;
+
+namespace TcpHtmlVerify
+{
+    public class UriParsing : Pattern
+    {
+        //private readonly string uriText;
+        private readonly Pattern pattern;
+
+        public UriParsing()
+        {
+            pattern = new Many(
+                new Choice(
+                    new Range('!', '9'),
+                    new Range(';', '~')));
+        }
+
+        public (Match, string) Match(string input)
+        {
+            var (match, remaining) = pattern.Match(input);
+            if (Uri.TryCreate(input, UriKind.Absolute, out Uri outUri))
+            {
+                return (match, remaining);
+            }
+            return (match, input);
+            
+        }
+    }
+}
---
+++ TcpTextVerifyTests/UriParsingTests.cs
@@ -0,0 +1,16 @@
+﻿using Xunit;
+
+namespace TcpHtmlVerify
+{
+    public class UriParsingTests
+    {
+        [Fact]
+        public void UriParsingTestCorrectUrl()
+        {
+            var uriText = "/en-us/library/system.uri(v=vs.110).aspx";
+            var pattern = new UriParsing();
+            var (match, remaining) = pattern.Match(uriText);
+            Assert.True(match.Success);
+        }
+    }
+}
