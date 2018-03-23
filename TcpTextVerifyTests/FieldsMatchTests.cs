ea263953fceed3df936baa8dd4f57afbd59c41b6
ionescuradu <ionescu_radu@ymail.com>
2018-03-23 11:35:37 +0200

    added new test for FieldsMatch class

--- TcpTextVerifyTests/FieldsMatchTests.cs
+++ TcpTextVerifyTests/FieldsMatchTests.cs
@@ -6,11 +6,22 @@ namespace TcpHtmlVerify
     public class FieldsMatchTests
     {
         [Fact]
-        public void KeyValuePairTest1()
+        public void FieldsMatchSuccess()
         {
             var keyValueMatch = new FieldsMatch(
                 "Server: nginx/1.13.9\r\nContent-Lenght: 112\r\n\r\n");
             Assert.True(keyValueMatch.Success);
+        }
+
+        [Fact]
+        public void FieldsMatchDictionary()
+        {
+            var keyValueMatch = new FieldsMatch(
+                "Server: nginx/1.13.9\r\nContent-Lenght: 112\r\n\r\n");
+            Dictionary<string, string> dictionaryToCompare = new Dictionary<string, string>();
+            dictionaryToCompare.Add("Server", "nginx/1.13.9");
+            dictionaryToCompare.Add("Content-Lenght", "112");
+            Assert.Equal(dictionaryToCompare,keyValueMatch.Dictionary);
         }
 
     }
