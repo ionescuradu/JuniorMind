c4a72335bfe4b5f4f91cd5c57db48685f6f91aa5
ionescuradu <ionescu_radu@ymail.com>
2018-03-23 12:40:28 +0200

    added second test for FieldsParsing

--- TcpTextVerifyTests/FiledsParsingTests.cs
+++ TcpTextVerifyTests/FiledsParsingTests.cs
@@ -1,12 +1,24 @@
-﻿using Xunit;
+﻿using JsonTests;
+using Xunit;
 namespace TcpHtmlVerify
 {
     public class FieldsParsingTests
     {
         [Fact]
+        public void FieldsParsing2Line()
+        {
+            var fieldText = "Server: nginx/1.13.9\n" +
+                "Content-Length: 3643";
+            var pattern = new FieldsParsing();
+            var (match, remaining) = pattern.Match(fieldText);
+            Assert.True(match.Success);
+            Assert.Equal("", remaining);
+        }
+
+        [Fact]
         public void FieldsParsing1Line()
         {
-            var fieldText = "Server: nginx/1.13.9 \n Content-Lenght: 112";
+            var fieldText = "Server: nginx/1.13.9";
             var pattern = new FieldsParsing();
             var (match, remaining) = pattern.Match(fieldText);
             Assert.True(match.Success);
