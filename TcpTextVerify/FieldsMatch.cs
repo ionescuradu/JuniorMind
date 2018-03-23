623ef3cde067d0fb7ac1fe064a00fd431b2aaabe
ionescuradu <ionescu_radu@ymail.com>
2018-03-23 11:29:03 +0200

    renamed 2 classes from KeyValuePairs to FieldsMatch, and added new test for this class

---
+++ TcpTextVerify/FieldsMatch.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using JsonTests;
+
+namespace TcpHtmlVerify
+{
+    public class FieldsMatch: Match
+    {
+        private readonly Dictionary<string, string> dictionary;
+
+        public FieldsMatch(string input)
+        {
+            var x = input.Split(":".ToCharArray());
+            dictionary = input
+                .Split("\n".ToCharArray())
+                .Where(s => !string.IsNullOrEmpty(s.Trim()))
+                .Select(s => s.Split(":".ToCharArray()))
+                .ToDictionary(s => s[0].Trim(), s => s[1].Trim());
+        }
+
+        public Dictionary<string, string> Dictionary => dictionary;
+
+        public bool Success => true;
+    }
+}
---
+++ TcpTextVerifyTests/FieldsMatchTests.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using Xunit;
+
+namespace TcpHtmlVerify
+{
+    public class FieldsMatchTests
+    {
+        [Fact]
+        public void KeyValuePairTest1()
+        {
+            var keyValueMatch = new FieldsMatch(
+                "Server: nginx/1.13.9\r\nContent-Lenght: 112\r\n\r\n");
+            Assert.True(keyValueMatch.Success);
+        }
+
+    }
+}
