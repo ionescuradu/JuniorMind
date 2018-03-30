2bf3a28ccaf37bfd4e58afd61e5c65c4b7bb2e43
ionescuradu <ionescu_radu@ymail.com>
2018-03-30 10:15:35 +0300

    added new class to implement IRepository

---
+++ TcpTextVerify/MemoRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.IO;
+using System.Text;
+
+namespace TcpHtmlVerify
+{
+    class MemoRepository : IRepository
+    {
+        public Stream Repository(string input)
+        {
+            return new MemoryStream(Encoding.ASCII.GetBytes(input));
+        }
+    }
+}
